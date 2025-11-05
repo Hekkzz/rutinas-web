ef6a56f07c0a379f6d05080f6d33df099c446cdb
Hekkzz <116587828+Hekkzz@users.noreply.github.com>
2025-11-04 23:52:58 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  menuToggle.classList.toggle("active");
+});
+
+// Cerrar el menú al hacer clic en un enlace
+menu.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+    menuToggle.classList.remove("active");
+  });
+});
+
 function redirigirLogin() {
   window.location.href = "login.html";
 }
